9ca612697d134474c03168b6839702060bd37f1c
Liakhov-Serg <112199980+Liakhov-Serg@users.noreply.github.com>
2022-12-21 13:40:25 +0200

    task 1
    13-40

---
+++ js/task-01.js
@@ -0,0 +1,9 @@
+const ulElem = document.querySelector(`ul#categories`);
+const countOfChildren = ulElem.children.length;
+console.log(`Number of categories: ${countOfChildren}`);
+
+ 
+
+const itemList = [...document.querySelectorAll(`.item`)];
+
+

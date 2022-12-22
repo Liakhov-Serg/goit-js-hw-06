500f0c0d11d7d249f8e028eb0cb77398b1b4a443
Liakhov-Serg <112199980+Liakhov-Serg@users.noreply.github.com>
2022-12-23 00:18:18 +0200

    Update task-05.js
    Good

--- js/task-05.js
+++ js/task-05.js
@@ -1,6 +1,8 @@
 const textInput = document.querySelector("#name-input");
+
 const placeholder = document.querySelector("#name-output");
-textInput.addEventListener("input", (event) =>{
+
+textInput.addEventListener("input", (event) => {
     if(event.currentTarget.value) {
         placeholder.textContent = event.currentTarget.value;
     }

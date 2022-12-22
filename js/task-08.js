e8f77a59894af6639b019771aaa79a1dda414c42
Liakhov-Serg <112199980+Liakhov-Serg@users.noreply.github.com>
2022-12-22 21:17:44 +0200

    Update task-08.js
    Good

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const userform = document.querySelector(".login-form");
 
+
+userform.addEventListener("submit", forFormSubmit);
+
+function forFormSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password }
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    alert("Enter the fields completely");
+    return console.log("Please fill in all the fields!");
+  }
+
+  console.log(`Email: ${Email.value}, Password: ${password.value}`);
+  event.currentTarget.reset();
+}

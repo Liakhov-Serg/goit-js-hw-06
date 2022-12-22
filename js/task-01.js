b78ec74252720a197252486f634db79e24b2bd78
Liakhov-Serg <112199980+Liakhov-Serg@users.noreply.github.com>
2022-12-22 04:22:44 +0200

    on change
    4-22

--- js/task-01.js
+++ js/task-01.js
@@ -7,3 +7,18 @@ console.log(`Number of categories: ${countOfChildren}`);
 const itemList = [...document.querySelectorAll(`.item`)];
 
 
+
+const itemArr = itemList.map((item) => {
+    const title = [...item.children];
+
+const itemObj = {};
+itemObj[title[0].textContent] = [...title[1].children].map(item => item.textContent);
+
+return itemObj;
+
+    });
+    console.log(itemArr);
+
+
+
+
--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,18 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+ { name: 'Potatoes'},
+  { name: 'Mushrooms'},
+  { name:'Garlic'},
+  { name:'Tomatos'},
+  { name:'Herbs'},
+  {name:'Condiments'},
 ];
+const list = document.querySelector("ul#ingredients");
+// console.log(list);
+ingredients.forEach("ingredient") => {
+const li = document.createElement("li");
+const name = document.createElement("p");
+name.textContent = ingredient.name;
+
+li.appendChild(name);
+console.log(li);
+};

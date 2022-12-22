80b7deb50ebdd83b86d0507ccf52efd3fe575d29
Liakhov-Serg <112199980+Liakhov-Serg@users.noreply.github.com>
2022-12-22 13:01:00 +0200

    norm
    13-00

--- js/task-03.js
+++ js/task-03.js
@@ -20,9 +20,9 @@ const list = document.querySelector(".gallery");
 console.log(list);
 
 images.forEach((image) => {
-  const markup = `<li><img width=100 src="{url:'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-}"/><></li>`;
-  list.insertAdjacentHTML("beforeend", markup);
+  const markup = `<li><img src="${image.url}" alt="${image.alt}" width = "200px"></li>`;
+  console.log(markup);
+  console.log(list.insertAdjacentHTML("afterbegin", markup));
 }
 
 );
---
+++ js/task-04.js
@@ -0,0 +1,13 @@
+const div = document.querySelector("id#counter");
+console.log("id#counter");
+const button = document.querySelector(".decrement");
+console.log(".decrement");
+const span = document.querySelector("id#value");
+console.log("id#value");
+// const button = document.querySelector(".increment");
+// console.log(".increment");
+button.addEventListener("click", () => {
+    console.log(+1);
+}
+
+)

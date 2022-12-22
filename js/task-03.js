bda14dafc708e524f811d7fad3f823cd0530fbea
Liakhov-Serg <112199980+Liakhov-Serg@users.noreply.github.com>
2022-12-22 06:05:19 +0200

    Update task-03.js
    5-50

--- js/task-03.js
+++ js/task-03.js
@@ -1,14 +1,28 @@
 const images = [
   {
+    
     url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     alt: 'White and Black Long Fur Cat',
   },
   {
+    
     url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
   },
   {
+    
     url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     alt: 'Group of Horses Running',
   },
 ];
+const list = document.querySelector(".gallery");
+
+console.log(list);
+
+images.forEach((image) => {
+  const markup = `<li><img width=100 src="{url:'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+}"/><></li>`;
+  list.insertAdjacentHTML("beforeend", markup);
+}
+
+);

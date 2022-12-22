ebec5d08fb13c4d9a84a96a49dbd7ce784d612ad
Liakhov-Serg <112199980+Liakhov-Serg@users.noreply.github.com>
2022-12-23 00:20:07 +0200

    Update task-06.js
    Good

--- js/task-06.js
+++ js/task-06.js
@@ -3,13 +3,13 @@ console.log(textInput);
 
 textInput.addEventListener("blur", () => {
     console.log(textInput.value.length);
+
     if(textInput.value.length === 6) {
         textInput.classList.add("valid");
         textInput.classList.remove("invalid")
         console.log("valid");
-
-    }
-    else {textInput.classList.remove("valid");
+    } 
+        else {textInput.classList.remove("valid");
            textInput.classList.add("invalid");
            console.log("invalid");
         }
@@ -17,18 +17,3 @@ textInput.addEventListener("blur", () => {
 
 })
 
-// const textInput = document.querySelector("#validation-input");
-// console.log(textInput);
-// textInput.addEventListener("blur", () => {
-//    if (textInput.value.length === Number(textInput.dataset.length)) {
-//     textInput.classList.add("valid");
-//    }
-//    else {
-//     textInput.classList.remove("valid");
-//     textInput.classList.add("invalid");
-//    }
-//   });
-
-// elem.classList.replace(oldClass, newClass)
-// elem.classList.add(cls) - додає клас cls до списку класів елемента.
-// elem.classList.remove(cls) - видаляє клас cls зі списку класів елемента.

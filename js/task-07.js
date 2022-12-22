6e7fd59a66570a94bb043d5db2d45507d572e3f3
Liakhov-Serg <112199980+Liakhov-Serg@users.noreply.github.com>
2022-12-23 00:21:54 +0200

    Update task-07.js
    Good

--- js/task-07.js
+++ js/task-07.js
@@ -2,7 +2,9 @@ const input = document.getElementById("font-size-control");
 console.log(input);
 const span = document.getElementById("text");
 console.log(span);
+
 input.addEventListener("input", handleInputRange);
+
 function handleInputRange(event) {
     span.style.fontSize = event.currentTarget.value+"px";
 }

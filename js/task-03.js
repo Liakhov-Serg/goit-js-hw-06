fdec75d2b8382754c6b4a663bf0c370867bee52e
Liakhov-Serg <112199980+Liakhov-Serg@users.noreply.github.com>
2022-12-23 00:15:19 +0200

    Update task-03.js
    Good

--- js/task-03.js
+++ js/task-03.js
@@ -1,16 +1,13 @@
 const images = [
   {
-    
     url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     alt: 'White and Black Long Fur Cat',
   },
   {
-    
     url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
   },
   {
-    
     url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     alt: 'Group of Horses Running',
   },
@@ -20,11 +17,12 @@ const list = document.querySelector(".gallery");
 console.log(list);
 
 images.forEach((image) => {
-  const markup = `<li><img src="${image.url}" alt="${image.alt}" width = "300px"></li>`;
+  const markup = `<li><img src="${image.url}" 
+                   alt="${image.alt}" width = "300px"></li>`;
+
   console.log(markup);
   console.log(list.insertAdjacentHTML("afterbegin", markup));
 }
-
 );
 
 list.style.display = "grid";

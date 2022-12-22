f4051adf677699a1987ccf588982bb97561b05bd
Liakhov-Serg <112199980+Liakhov-Serg@users.noreply.github.com>
2022-12-23 00:12:04 +0200

    Update task-01.js
    Good

--- js/task-01.js
+++ js/task-01.js
@@ -2,17 +2,14 @@ const ulElem = document.querySelector(`ul#categories`);
 const countOfChildren = ulElem.children.length;
 console.log(`Number of categories: ${countOfChildren}`);
 
- 
-
 const itemList = [...document.querySelectorAll(`.item`)];
-
-
 
 const itemArr = itemList.map((item) => {
     const title = [...item.children];
 
 const itemObj = {};
-itemObj[title[0].textContent] = [...title[1].children].map(item => item.textContent);
+itemObj[title[0].textContent] = 
+[...title[1].children].map(item => item.textContent);
 
 return itemObj;
 

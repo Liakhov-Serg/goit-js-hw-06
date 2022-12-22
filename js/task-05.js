ebd01fd57de0999370f96d7d267e306c7c02ecfa
Liakhov-Serg <112199980+Liakhov-Serg@users.noreply.github.com>
2022-12-22 23:57:10 +0200

    Update task-05.js
    Good

---
+++ js/task-05.js
@@ -0,0 +1,8 @@
+const textInput = document.querySelector("#name-input");
+const placeholder = document.querySelector("#name-output");
+textInput.addEventListener("input", (event) =>{
+    if(event.currentTarget.value) {
+        placeholder.textContent = event.currentTarget.value;
+    }
+    else {placeholder.textContent = "Anonymous";}
+} );

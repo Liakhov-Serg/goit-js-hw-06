479d55eb977e46c88fa7efa2afd39f7ee220f4ed
Liakhov-Serg <112199980+Liakhov-Serg@users.noreply.github.com>
2022-12-23 18:13:43 +0200

    Update task-09.js
    Good

--- js/task-09.js
+++ js/task-09.js
@@ -2,13 +2,13 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 const btnChangeColor = document.querySelector(".change-color");
-console.log("change-color");
+
 
 const getBody = document.querySelector("body");
-console.log("body");
+
 
 const changeColor = document.querySelector(".color");
-console.log("color");
+
 
 btnChangeColor.addEventListener("click", () => {
   const currentColor = getRandomHexColor();

3854476e88ea089fe88c2ddfc6e993875b68820e
Liakhov-Serg <112199980+Liakhov-Serg@users.noreply.github.com>
2022-12-23 18:10:55 +0200

    Update task-07.js
    Good

--- js/task-07.js
+++ js/task-07.js
@@ -1,7 +1,7 @@
 const input = document.getElementById("font-size-control");
-console.log(input);
+
 const span = document.getElementById("text");
-console.log(span);
+
 
 input.addEventListener("input", handleInputRange);
 
